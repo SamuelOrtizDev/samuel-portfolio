33eeca99ff846db1b8cb24b2b41196ad48d24bc4
SamueLauXd <ortizsamuel2004@hotmail.com>
2024-04-21 10:28:55 -0500

    cv button font medium

--- src/components/BrandButton.jsx
+++ src/components/BrandButton.jsx
@@ -1,6 +1,6 @@
 export function BrandButton ({children, href = '#'}) {
     return (
-        <a href={href} target="__blank" className="px-4 py-2 text-light flex gap-3 hover:border-purple hover:text-orange bg-blue transition-colors ease-in-out duration-300 rounded-full items-center">
+        <a href={href} target="__blank" className="px-4 py-2 text-light font-medium flex gap-3 hover:border-purple hover:text-orange bg-blue transition-colors ease-in-out duration-300 rounded-full items-center">
             {children}
         </a>
     )
