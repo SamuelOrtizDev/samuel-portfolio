ff404b06bd7278c2f6e3e5cb8522066d138e3979
SamuelOrtizDev <ortizsamuel2004@hotmail.com>
2024-11-11 14:14:36 -0500

    added job experience

---
+++ src/components/Experience.jsx
@@ -0,0 +1,51 @@
+const portfolioData = [
+  {
+    company: "Sports Funded",
+    title: "Frontend Developer",
+    date: "Jun 2024 - Present",
+    description: "Developed responsive web applications using React and Next.js.",
+  },
+  {
+    company: "TopTier Trader",
+    title: "Junior Frontend Developer",
+    date: "Jun 2024 - Nov 2024",
+    description: "Developed responsive web pages using React and Next.js. Implemented state management solutions and optimized performance. Implemented new pages, sections and blogs.",
+  },
+  {
+    company: "Freelance",
+    title: "UI Designer / Frontend Developer",
+    date: "Nov 2023 - Jun 2024",
+    description: "Desgined web interfaces for corporative Landing Pages. Developed sections and web pages. Integrated translations features",
+  },
+  {
+    company: "enerBit",
+    title: "UX/UI Designer",
+    date: "Aug 2023 - Dec 2024",
+    description: "Desgined mobile interfaces. Conduct user testing. Create surveys to gauge user satisfaction. Produce visual assets",
+  },
+]
+
+function ExperienceItem({ item }) {
+  return (
+    <li className="bg-card shadow-md rounded-lg overflow-hidden">
+      <div className="p-6">
+        <h3 className="text-xl font-bold mb-4">{item.company}</h3>
+        <div className="flex flex-col sm:flex-row sm:justify-between mb-4">
+          <span className="font-semibold text-purple">{item.title}</span>
+          <span>{item.date}</span>
+        </div>
+        <p>{item.description}</p>
+      </div>
+    </li>
+  )
+}
+
+export function Experience() {
+  return (
+    <ul className="flex flex-col gap-8">
+      {portfolioData.map((item, index) => (
+        <ExperienceItem key={index} item={item} />
+      ))}
+    </ul>
+  )
+}
