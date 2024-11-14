76dc802f88de992da143d4899eec4922d2bf83e6
SamuelOrtizDev <ortizsamuel2004@hotmail.com>
2024-11-14 14:45:42 -0500

    className fixed on contact form

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -10,11 +10,11 @@ export function ContactForm() {
 
     return (
         <>
-            <h5 class="font-bold text-xl md:text-2xl">Wanna Talk?</h5>
+            <h5 className="font-bold text-xl md:text-2xl">Wanna Talk?</h5>
             <p>Submit your email to get in touch</p>
             <form onSubmit={handleSubmit} class="flex flex-wrap gap-2 items-center">
                 <input
-                    class="px-2 py-1 bg-card rounded-md border border-transparent outline-none focus:border-blue"
+                    className="px-2 py-1 bg-card rounded-md border border-transparent outline-none focus:border-blue"
                     required
                     placeholder="example@example.com"
                     id='email'
@@ -25,7 +25,7 @@ export function ContactForm() {
                 <button
                     type="submit"
                     disabled={state.submitting}
-                    class="px-2 py-1 bg-blue rounded-md transition-all ease-in-out duration-200 hover:shadow-lg hover:shadow-blue/90"
+                    className="px-2 py-1 bg-blue rounded-md transition-all ease-in-out duration-200 hover:shadow-lg hover:shadow-blue/90"
                 >Submit</button
                 >
             </form>
