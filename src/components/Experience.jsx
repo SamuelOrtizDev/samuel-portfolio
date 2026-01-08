3c437921bdeac839f04e8680b0c0d7f92bcc408a
Samuel Ortiz <samuelortizwork4@gmail.com>
2026-01-08 11:57:30 -0500

    feat: update experience and projects

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -6,9 +6,15 @@ const experience = [
     description: "Developed responsive web applications using React and Vanilla JS",
   },
   {
+    company: "OneOfOne Posters",
+    title: "Frontend Engineer",
+    date: "Aug 2025 - Present",
+    description: "Developed responsive web applications using React and Next.js.",
+  },
+  {
     company: "Sports Funded",
     title: "Frontend Developer",
-    date: "Jun 2024 - Present",
+    date: "Jun 2024 - Jun 2025",
     description: "Developed responsive web applications using React and Next.js.",
   },
   {
@@ -16,19 +22,7 @@ const experience = [
     title: "Junior Frontend Developer",
     date: "Jun 2024 - Nov 2024",
     description: "Developed responsive web pages using React and Next.js. Implemented state management solutions and optimized performance. Implemented new pages, sections and blogs.",
-  },
-  {
-    company: "Freelance",
-    title: "UI Designer / Frontend Developer",
-    date: "Nov 2023 - Jun 2024",
-    description: "Desgined web interfaces for corporative Landing Pages. Developed sections and web pages. Integrated translations features",
-  },
-  {
-    company: "enerBit",
-    title: "UX/UI Designer",
-    date: "Aug 2023 - Dec 2024",
-    description: "Desgined mobile interfaces. Conduct user testing. Create surveys to gauge user satisfaction. Produce visual assets",
-  },
+  }
 ]
 
 function ExperienceItem({ item }) {
